381e84883348444aaf2b5256b42eecb80f170789
Vitaliy Viktorovich <po4tavela@gmail.com>
2021-12-18 15:50:04 +0300

    added menu and minor fixes

--- Talalay_VV_C/main.c
+++ Talalay_VV_C/main.c
@@ -14,14 +14,50 @@ void task3(void);
 void task4(void);
 void task5(void);
 
+void menu(void);
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    task1();
-    task2();
-    task3();
-    task4();
-    task5();
+    int taskNumber = 0;
+    
+    do {
+        menu();
+        scanf("%i",&taskNumber);
+        switch (taskNumber) {
+            case 1:
+                task1();
+                break;
+            case 2:
+                task2();
+                break;
+            case 3:
+                task3();
+                break;
+            case 4:
+                task4();
+                break;
+            case 5:
+                task5();
+                break;
+            case 0:
+                printf("Программа завершена, всего доброго!\n");
+                break;
+            default:
+                printf("Такого задания нет\n");
+                break;
+        }
+    } while (taskNumber != 0);
+    
     return 0;
+}
+
+void menu() {
+    printf("Введите номер задания:\n");
+    printf("'1' - Подсчет BMI\n");
+    printf("'2' - Определение максимального числа из 4х\n");
+    printf("'3' - ОБмен двух значений\n");
+    printf("'4' - Нахождение корней квадратного уравнения\n");
+    printf("'5' - Определение времени года\n");
+    printf("'0' - Выход\n");
 }
 
 /*
@@ -90,26 +126,36 @@ int main(int argc, const char * argv[]) {
      b = a - b;
      a = a - b;
      printf("Значения после повторного обмена:\n a = %i, b = %i \n", a, b);
+     a = a - b;
+     b = a + b;
+     a = b - a;
+     printf("Значения после повторного обмена:\n a = %i, b = %i \n", a, b);
+     a = a * b;
+     b = a / b;
+     a = a / b;
+     printf("Значения после повторного обмена:\n a = %i, b = %i \n", a, b);
  }
 
 // 4. Написать программу нахождения корней заданного квадратного уравнения.
 
  void task4() {
-     double a, b, c, d, x1, x2;
+     double a, b, c, D, x1, x2;
+     printf("******************\n");
+     printf("Задача 4\n");
      printf("Введите коэффициент a \n");
      scanf("%lf", &a);
      printf("Введите коэффициент b \n");
      scanf("%lf", &b);
      printf("Введите коэффициент c \n");
      scanf("%lf", &c);
-     d = (b * b) - (4 * a * c);
-     if (d < 0) {
+     D = pow(b,2) - (4 * a * c);
+     if (D < 0) {
          printf("Корней нет \n");
-     } else if (d == 0) {
+     } else if (D == 0) {
          x1 = -b / (2 * a);
          printf("Один корень: %lf \n", x1);
-     } else if (d > 0) {
-         double sd = sqrt(d);
+     } else if (D > 0) {
+         double sd = sqrt(D);
          x1 = (-b + sd) / (2 * a);
          x2 = (-b - sd) / (2 * a);
          printf("Корней два: \n x1 = %lf \n x2 = %lf \n", x1, x2);
@@ -123,18 +169,20 @@ int main(int argc, const char * argv[]) {
 
  void task5() {
      int n;
+     printf("******************\n");
+     printf("Задача 5\n");
      printf("Введите номер месяца n \n");
      scanf("%i", &n);
 
      if (n == 1 || n == 2 || n == 12) {
-         printf("Зима");
+         printf("Месяц относится к зиме");
      } else if (n == 3 || n == 4 || n == 5) {
-         printf("Весна");
+         printf("Месяц относится к весне");
      } else if (n == 6 || n == 7 || n == 8) {
-         printf("Лето");
+         printf("Месяц относится к лету");
      } else if (n == 9 || n == 10 || n == 11) {
-         printf("Осень");
+         printf("Месяц относится к осени");
      } else {
-         printf("Такого месяца нет");
+         printf("Не правильно введен номер месяца\n");
      }
  }
