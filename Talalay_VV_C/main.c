7bdd8d585d92ba00b49a1c0ec7d4129527cf8ffe
Vitaliy Viktorovich <po4tavela@gmail.com>
2021-12-18 13:54:06 +0300

    create repository

---
+++ Talalay_VV_C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Talalay_VV_C
+//
+//  Created by Vitaliy Talalay on 18.12.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
